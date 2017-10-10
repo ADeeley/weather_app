74dd67bb5612351af68274efda62046a7a48bb6a
Adam M Deeley <ad.deeley@gmail.com>
2017-10-10 21:05:46 +0100

    Added icon for the weather

--- main.js
+++ main.js
@@ -1,21 +1,48 @@
-var requestURL = "https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139";
+window.onload = function() {
+    /**
+     * returns a JSON object from the specified URL
+     */
+    var requestURL = "https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139";
 
-// Declare a new request object
-var request = new XMLHttpRequest();
+    // Declare a new request object
+    var request = new XMLHttpRequest();
 
-// Open a new request 
-request.open('GET', requestURL);
+    // Open a new request 
+    request.open('GET', requestURL);
 
-// Declare the data type to convert the URL response data to
-request.responseType = 'json';
+    // Declare the data type to convert the URL response data to
+    request.responseType = 'json';
 
-// Send the request to the URL provided
-request.send();
+    // Send the request to the URL provided
+    request.send();
 
-// Wait for the request to fetch the JSON, then do the following
-request.onload = function() {
-    var data = request.response;
-    console.log(data);
+    // Wait for the request to fetch the JSON, then do the following
+    request.onload = function() {
+        updateHTML(request.response);
+    }
 }
+
+function updateHTML(data) {
+    //document.getElementById("weather_type").innerHTML = data["weather"][0]["main"];
+    // Create weather html
+    var header = document.createElement("h2");
+    var text = document.createTextNode(data["weather"][0]["main"]);
+    header.appendChild(text);
+    header.id = "weather_type";
+    document.body.appendChild(header);
+    
+    // Create weather html
+    var icon = document.createElement("img");
+    icon.id = "weather_icon";
+    icon.src = data["weather"][0]["icon"]; 
+    document.body.appendChild(icon);
+}
+
+
+
+
+
+
+
 
 
