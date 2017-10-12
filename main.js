569a7ca6c7dd9bbb4ae8296a6062e69fb64139f9
Adam M Deeley <ad.deeley@gmail.com>
2017-10-12 19:05:05 +0100

    Working Celsius to Fahrenheit.

--- main.js
+++ main.js
@@ -1,62 +1,119 @@
+var JSONdata = null;
+var isCelsius = true;
+
 window.onload = function() {
-    /**
-     * returns a JSON object from the specified URL
-     */
-    var requestURL = "https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139";
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            /**
+             * returns a JSON object from the specified URL
+             */
+            var requestURL = "https://fcc-weather-api.glitch.me/api/current?";
+            var lat = "lat=" + position.coords.latitude;
+            var lon = "&lon=" + position.coords.longitude;
 
-    // Declare a new request object
-    var request = new XMLHttpRequest();
+            // Declare a new request object
+            var request = new XMLHttpRequest();
 
-    // Open a new request 
-    request.open('GET', requestURL);
+            // Open a new request 
+            request.open('GET', requestURL + lat + lon);
 
-    // Declare the data type to convert the URL response data to
-    request.responseType = 'json';
+            // Declare the data type to convert the URL response data to
+            request.responseType = 'json';
 
-    // Send the request to the URL provided
-    request.send();
+            // Send the request to the URL provided
+            request.send();
 
-    // Wait for the request to fetch the JSON, then do the following
-    request.onload = function() {
-        updateHTML(request.response);
+            // Wait for the request to fetch the JSON, then do the following
+            request.onload = function() {
+                updateHTML(request.response);
+            }
+        });
+    }
+    else {
+        console.log("Geolocation is not available in this browser.");
     }
 }
 
 function updateHTML(data) {
-    //document.getElementById("weather_type").innerHTML = data["weather"][0]["main"];
-    // Create weather html
-    var header = document.createElement("h2");
-    var text = document.createTextNode("Weather: " + data["weather"][0]["main"]);
-    header.appendChild(text);
-    header.id = "weather_type";
-    header.classList.add("data");
-    document.body.appendChild(header);
-    
-    // Create temperature html
-    var header = document.createElement("h3");
-    var text = document.createTextNode("Temperature: " + data["main"]["temp"] + " Celsius");
-    header.appendChild(text);
-    header.id = "temperature";
-    header.classList.add("data");
-    document.body.appendChild(header);
+    /*
+     * Create and populate the tags using the JSON data
+     */
+    var text = "Weather: " + data["weather"][0]["main"];
+    document.getElementById("weather_type").innerHTML = text;
+    text = "Temperature: " + data["main"]["temp"];
+    document.getElementById("temperature").innerHTML = text + " Celsius";
+    text = "Wind Speed: " + data["wind"]["speed"] + " mph";
+    document.getElementById("wind_speed").innerHTML = text;
 
-    // Create wind html
-    var header = document.createElement("h3");
-    var text = document.createTextNode("Wind Speed: " + data["wind"]["speed"] + " mph");
-    header.appendChild(text);
-    header.id = "temperature";
-    header.classList.add("data");
-    document.body.appendChild(header);
-
-    // Create icon 
+    // Create weather icon 
     var icon = document.createElement("img");
     icon.id = "weather_icon";
     icon.src = data["weather"][0]["icon"]; 
     document.body.appendChild(icon);
+
+    adaptBackgroundColour(data["weather"][0]["main"]);
+
+    // store the JSON in JSONdata global
+    JSONdata = data;
+
 }
 
+function adaptBackgroundColour(weatherType) {
+    var col = "background-color: ";
+    switch (weatherType) {
+        case "Rain":
+            col += "#3D66FF";
+            break;
+        case "Sunny":
+            col += "#f4dc41";
+            break;
+        case "Drizzle":
+            col += "#9AA9E0";
+            break;
+        case "Clouds":
+            col += "#DCE0ED";
+            break;
+        case "Snow":
+            col += "#ffffff";
+            break;
+        case "clear":
+            col += "#75C6F4";
+            break;
+        case "thunderstorm":
+            col += "#161E4C";
+            break;
+        default:
+            col += "#abcdef";
+    }
+    document.body.style = col;
+}
 
+function convertTempScale(num) {
+    if (isCelsius) {
+        return Math.floor((num * 1.8) + 32);
+    }
+    else if (!isCelsius) {
+        return (num - 32) ;
+    }
+    else {
+        return null;
+    }
+}
 
+function changeScale() {
+    var num = JSONdata["main"]["temp"];
+    if (isCelsius) {
+       num = convertTempScale(num);
+       document.getElementById("temperature").innerHTML = "Temperature: " + num + " Fahrenheit";
+        isCelsius = false;
+    }
+    else if (!isCelsius) {
+       num = convertTempScale(num);
+       document.getElementById("temperature").innerHTML = "Temperature: " + JSONdata["main"]["temp"] + " Celsius";
+        isCelsius = true;
+    }
+    //Update HTML using DOM
+}
 
 
 
